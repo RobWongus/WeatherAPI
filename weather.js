2bf16ad397846e31cb53c163676eafc86870eb86
Robert Wongus <rob_wo@yahoo.com>
2020-03-03 20:07:22 -0500

    button sync and added bootstrap forms

--- weather.js
+++ weather.js
@@ -1,1 +1,21 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    let citySearch = document.getElementById("citysearch")
+    
+    citySearch.addEventListener("click", clickButton)
+    
+    
+    
+    function clickButton(event) {
+        let searchBar = event.target.previousElementSibling.value
+        let node = document.createElement("list-group");                 // Create a <li> node
+        let textnode = document.createTextNode("");         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        document.getElementsByClassName("myList").appendChild(node);
+        event.preventDefault();
+        
+        
+    }
+    
+});
+
 
