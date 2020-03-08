acce85429124ce2b0ae98cee6b6bde62dab5ba06
Robert Wongus <rob_wo@yahoo.com>
2020-03-08 19:19:18 -0400

    adding button

--- weather.js
+++ weather.js
@@ -1,13 +1,59 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    const weatherKey = "b2e836d390c1f7505f5a7127639e2867"
-    
-    const URL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=" + key;
-    //UV Index//
-    const URLUV = "https://api.openweathermap.org/data/2.5/uvi?appid=" + key + "&lat=" + lat + "&lon=" + lon;
-
+    let button = document.getElementById("citysearch");
+    let cityEntry = document.getElementsByClassName("form-control");
+    const key = "b2e836d390c1f7505f5a7127639e2867";
+    const form = document.getElementById("forminput");
     //Current Weather
-    const currentWeather = "api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}"
-
+    const weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={key}";
+    //UV Index//
+    const URLUV = "http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}";
+    
+    
     //Five Day Forecast, Future Forecast
-    const futureWeather = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}"
+    const futureWeather = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={key}"
+    // function displayWeather() {
+    //     document.getElementById(citySearch) .click
+        
+    // }
+
+    function clickMe() {
+        document.getElementById("clickMe").innerHTML=cityList
+        console.log(cityList)
+    }
+    function addItem(){
+        var li = document.createElement("LI");  
+        var input = document.getElementById("add");
+        li.innerHTML = input.value;
+        input.value = "";
+        document.getElementById("cityList").appendChild(li);
+    }
+    // const weather = {
+    //     temperature : {
+    //         value: ,
+    //         unit : "celsius"
+    //         humidity :
+    //         windspeed
+    //         uvindex
+    //     },
+    //     decsription : ""
+    //     iconId : ""
+    //     city : "",
+    //     country : ""
+    // }
+    
+    // citySearch.addEventListener("click", clickButton)
+    
+    
+    
+    // function clickButton(event) {
+    //     let searchBar = event.target.previousElementSibling.value
+    //     let node = document.createElement("list-group");                 // Create a <li> node
+    //     let textnode = document.createTextNode("");         // Create a text node
+    //     node.appendChild(textnode);                              // Append the text to <li>
+    //     document.getElementsByClassName("myList").appendChild(node);
+    //     event.preventDefault();
+        
+        
+    // }
+    
 });
